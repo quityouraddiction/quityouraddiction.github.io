30a16d10f6c6133e51e1533a6bbc9c4af2b9e704
quityouraddiction <quityouraddiction@outlook.com>
2024-03-27 23:30:25 +0000

    added links to the featured content

--- src/components/featuredContent/featuredContent.js
+++ src/components/featuredContent/featuredContent.js
@@ -5,9 +5,10 @@ import { StaticImage } from "gatsby-plugin-image";
 const FeaturedContent = () => {
   return (
     <div class="container mx-auto px-4">
-      <h2 class="text-4xl font-bold text-left text-primary">Featured Content</h2>
+      <h2 class="text-4xl font-bold text-left text-primary">
+        Featured Content
+      </h2>
       <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12">
-
         <div class="featured-content-card overflow-hidden">
           <div class="relative p-4 text-right">
             <p class="text-gray-600 leading-relaxed">
@@ -23,59 +24,66 @@ const FeaturedContent = () => {
 
         <div class=" rounded-3xl shadow-lg overflow-hidden hover:shadow-2xl transition-transform duration-300 ease-in-out">
           <div class="relative">
-            <Link to="#">
+            <Link
+              to="https://www.youtube.com/watch?v=mN2KgCm_K1E"
+              target="_blank"
+              rel="noreferrer"
+            >
               <StaticImage
                 src="../../images/featured_tedx_talk.jpg"
-                alt="Featured Image 2"
+                alt="TedX Talk by Johann Hari"
                 class="w-full h-64 object-cover"
               />
+
+              <div class="inset-0 flex flex-col justify-center items-center text-center opacity-0 hover:opacity-90 transition-opacity duration-300 ease-in-out absolute  p-6 bg-primary">
+                <h3 class="text-xl font-semibold text-white mb-2">
+                  Everything you think you know about addiction is wrong -
+                  Johann Hari
+                </h3>
+              </div>
             </Link>
-            <div class="inset-0 flex flex-col justify-center items-center text-center opacity-0 hover:opacity-90 transition-opacity duration-300 ease-in-out absolute  p-6 bg-primary">
-              <h3 class="text-xl font-semibold text-white mb-2">
-                Featured Content Title
-              </h3>
-              <p class="text-gray-300">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              </p>
-            </div>
           </div>
         </div>
         <div class=" rounded-3xl shadow-lg overflow-hidden hover:shadow-2xl transition-transform duration-300 ease-in-out">
           <div class="relative">
-            <Link to="#">
+            <Link
+              to="https://www.youtube.com/watch?v=reclh_933RI"
+              target="_blank"
+              rel="noreferrer"
+            >
               <StaticImage
                 src="../../images/featured_youtube_interview.jpg"
                 alt="Featured Image 2"
                 class="w-full h-64 object-cover"
               />
+
+              <div class="inset-0 flex flex-col justify-center items-center text-center opacity-0 hover:opacity-90 transition-opacity duration-300 ease-in-out absolute  p-6 bg-primary">
+                <h3 class="text-xl font-semibold text-white mb-2">
+                  Dr Hurd on the Ken Hamblin Show
+                </h3>
+              </div>
             </Link>
-            <div class="inset-0 flex flex-col justify-center items-center text-center opacity-0 hover:opacity-90 transition-opacity duration-300 ease-in-out absolute  p-6 bg-primary">
-              <h3 class="text-xl font-semibold text-white mb-2">
-                Featured Content Title
-              </h3>
-              <p class="text-gray-300">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              </p>
-            </div>
           </div>
         </div>
         <div class=" rounded-3xl shadow-lg overflow-hidden hover:shadow-2xl transition-transform duration-300 ease-in-out">
           <div class="relative">
-            <Link to="#">
+            <Link
+              to="https://lnns.co/u-rDb9q4eWF"
+              target="_blank"
+              rel="noreferrer"
+            >
               <StaticImage
                 src="../../images/featured_spotify_podcast.jpg"
                 alt="Featured Image 2"
                 class="w-full h-64 object-cover"
               />
+
+              <div class="inset-0 flex flex-col justify-center items-center text-center opacity-0 hover:opacity-90 transition-opacity duration-300 ease-in-out absolute  p-6 bg-primary">
+                <h3 class="text-xl font-semibold text-white mb-2">
+                  17 Minutes to Heaven
+                </h3>
+              </div>
             </Link>
-            <div class="inset-0 flex flex-col justify-center items-center text-center opacity-0 hover:opacity-90 transition-opacity duration-300 ease-in-out absolute  p-6 bg-primary">
-              <h3 class="text-xl font-semibold text-white mb-2">
-                Featured Content Title
-              </h3>
-              <p class="text-gray-300">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              </p>
-            </div>
           </div>
         </div>
       </div>
